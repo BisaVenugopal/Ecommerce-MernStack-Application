c05a25f45f076ba58045d473de87d9afb583a800
Venugopal <karthivenu49@gmail.com>
2024-07-07 18:55:33 +0530

    Login and Logout

--- backend/controllers/user/userLogout.js
+++ backend/controllers/user/userLogout.js
@@ -1,5 +1,10 @@
 async function userLogout(req,res){
     try{
+        const tokenOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : 'None'
+         }
         res.clearCookie("token")
         res.json({
             message : "Logged out successfully.",
